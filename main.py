294aa0854b806dc7a182ea04293c6d845c45a5de
Georgiana Voicea <georgiana.voicea2001@gmail.com>
2022-03-08 12:51:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-import sys
 import re
-
-
 
 HOST = r'^(?P<host>.*?)'
 SPACE = r'\s'
@@ -16,59 +13,70 @@ SIZE = r'(?P<size>\S+)'
 
 regex = HOST+SPACE+IDENTITY+SPACE+USER+SPACE+TIME+SPACE+REQUEST+SPACE+STATUS+SPACE+SIZE+SPACE
 numberOfElements=0
-listIp=[]
-listHttp=[]
+myList=[]
 
-def OrderDescByBytes():
-    list = sorted(listIp, key = lambda x: (x[4]), reverse= True)
-    for line in list:
-        print(line)
-
-def GroupByIp():
-    for line in listIp:
-        print(line)
-
-def GroupByHttp():
-    for line in listHttp:
-        print(line)
-
-def RequestCount(request):
-    sum=0
-    for line in listIp:
-        if line[2]==request:
-            sum=sum+1
-    return sum
-
-def TotalNumberBytes():
-    sum = 0
-    for line in listIp:
-        sum+=line[4]
-    return sum
-
-def RowsPrinted(rowsNumber):
-    for line in listIp:
-        print(line)
-        rowsNumber-=1
-        if rowsNumber==0:
-            break
-
-def RequestPercentage():
-   ok = 0
-   listReq = []
-   for line in listIp:
-       for r in listReq:
-           if r == line[2]:
-               ok=1
+#--------------------------------------------------------------------------------------
+def GroupByIp(myList):
+   exists = False
+   listIp = []
+   for line in myList:
+       exists=False
+       for r in listIp:
+           if r[0] == line[0]:
+               exists=True
                break
-       if ok==0:
-           listReq.append(line[2])
-           print(f' {line[2]}  -  {RequestCount(line[2])/numberOfElements*100} %')
+       if exists==False:
+           listIp.append([line[0],0,0,0])
+   return listIp
+
+def GroupByHttp(myList):
+   exists = False
+   listHttp = []
+   for line in myList:
+       exists=False
+       for r in listHttp:
+           if r[0] == line[3]:
+               exists=True
+               break
+       if exists==False:
+           listHttp.append([line[3],0,0,0])
+   return listHttp
+
+#-----------------------------------------------------------------------
+def Count(list,position):
+    for line in myList:
+        for line2 in list:
+            if line[position]==line2[0]:
+                line2[1]=line2[1]+1
+                break
 
 
+def Percentage(list,position):
+    Count(list,position)
+    for line in list:
+        line[2]=line[1]*100/numberOfElements
+
+def BytesTransferred(list,position):
+    for line in list:
+        for line2 in myList:
+            if line[0]==line2[position]:
+                line[3]+=line2[4]
 
 
+def Print(rows,myList,i):
+    list = sorted(myList, key=lambda x: (x[i]), reverse=True)
+    if rows != "":
+        rows = int(rows)
+        for line in list:
+            print(f'{line[0]}: {line[i]}')
+            rows -= 1
+            if rows == 0:
+                break
+    else:
+        for line in list:
+            print(f'{line[0]}: {line[i]}')
 
-
+#-----------------------------------------------------------------------
 if __name__ == '__main__':
     filename = 'log'
     with open(filename, 'r') as logfile:
@@ -83,41 +91,38 @@ if __name__ == '__main__':
                 size = 0
             else:
                 size = int(match.group('size'))
-            listIp.append([ip,time,request,http,size])
-            listHttp.append([http,ip, time, request, size])
+            myList.append([ip,time,request,http,size])
 
-    while 1:
-        print(
-            "1- Group by Ip; \n2- Group by Http; \n3- Request count; \n4- Request count percentage of all logged requests; "
-            "\n5- Total number of bytes transferred;\n6-Print the results in descending order by bytes;\n7-Limit the number of rows printed;"
-            "\n0- End of program   ")
-        choise=input("Enter your choise: ")
-        if choise=='1':
-            GroupByIp()
-        elif choise=='2':
-            GroupByHttp()
-        elif choise=='3':
-            req=input("Request:")
-            try:
-                print(RequestCount(req))
-            except:
-                print("0")
-        elif choise=='4':
-            RequestPercentage()
-        elif choise=='5':
-            print(TotalNumberBytes())
-        elif choise=='6':
-            OrderDescByBytes()
-        elif choise=='7':
-            rowsNumber=input("The number of rows printed: ")
-            RowsPrinted(int(rowsNumber))
-        elif choise=='0':
-            print("End of program.")
-            break
+    listIp=GroupByIp(myList)
+    listHttp=GroupByHttp(myList)
 
-
-
-
-
-
-
+    while True:
+       choise=input("Enter your choise(ip or http or 0- End of program): ")
+       if choise=='0':
+           break
+       if choise=='ip':
+           request=input("1- Request count; \n2- Request count percentage of all logged requests \n3- Total number of bytes transferred;\n")
+           rows=input("Rows:")
+           position=0
+           if request=='1':
+               Count(listIp,position)
+               Print(rows,listIp,int(request))
+           if request == '2':
+               Percentage(listIp,position)
+               Print(rows, listIp, int(request))
+           if request == '3':
+               BytesTransferred(listIp,position)
+               Print(rows, listIp, int(request))
+       if choise=='http':
+           request = input("1- Request count; \n2- Request count percentage of all logged requests \n3- Total number of bytes transferred;\n")
+           rows = input("Rows:")
+           position=3
+           if request == '1':
+               Count(listHttp,position)
+               Print(rows, listHttp, int(request))
+           if request == '2':
+               Percentage(listHttp,position)
+               Print(rows, listHttp, int(request))
+           if request == '3':
+               BytesTransferred(listHttp,position)
+               Print(rows, listHttp, int(request))
